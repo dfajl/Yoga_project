340df00610e55014be9a43461e8071fcb9377f80
dfajl <dfajl@mail.ru>
2021-04-04 07:51:27 +0300

    tabs and timer created

--- js/yoga.js
+++ js/yoga.js
@@ -39,6 +39,75 @@ window.addEventListener('DOMContentLoaded', function() {
 
     });
 
+    // Timer
+
+    let deadline = '2021-04-21'; // конечная дата
+
+    function getTimeRemaining (endTime) {
+        let t = Date.parse(endTime) - Date.parse(new Date()), 
+            //отнимаем время в милисекундах от дэдлайна до текущей даты
+            seconds = Math.floor((t / 1000) % 60),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+            days = Math.floor((t / (1000 * 60 * 60 * 24)));
+
+        // чтобы получить дни: 
+        // hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+        // days = Math.floor((t / (1000 * 60 * 60 * 24)));
+        
+        
+        // так как из функции взять сразу несколько переменных мы не можем - вернем их в виде объекта и функции return
+
+        return {
+            'total': t, //полное количество милисекунд
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds,
+            'days': days
+        };
+    }
+  
+
+
+    function setClock (id, endTime) {             
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            days    = timer.querySelector('.days'),
+            timeInterval = setInterval(updateClock, 1000);
+
+
+        function updateClock () {
+            let t = getTimeRemaining (endTime);
+
+
+            function addZero (num) {
+                if (num <= 9) {
+                    return '0' + num; 
+                } else {
+                    return num;
+                }
+
+            }
+            hours.textContent = addZero(t.hours) + ' часов';
+            minutes.textContent = addZero(t.minutes) + ' минут';
+            seconds.textContent = addZero(t.seconds) + ' секунд';
+            days.textContent = addZero(t.days) + ' дней';
+
+            if (t.total <= 0) {
+                clearInterval (timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+                days.textContent = '00';
+            }
+        }    
+
+    }
+
+    setClock('timer', deadline); 
+    // эта функция выставляет и запускает наши часы
     
 });
 
